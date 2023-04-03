7efb7c72359c17a0e258770323cf321f29cb48e9
Asim-hameed <asimhameed31@gmail.com>
2023-04-03 19:45:25 +0500

    Implicit and Explicit implmentation of interface

--- INTERFACES/Program.cs
+++ INTERFACES/Program.cs
@@ -10,13 +10,27 @@
 
         void show();
     }
+
+
     interface i1
     {
-        void show1();
+        void show();
     }
-    interface i2 : i1, IEmployee
+    interface i2 
     {
-        void show2();
+        void show();
+    }
+    class MyClass : i1, i2
+    {
+        void i1.show()
+        {
+            Console.WriteLine("This is method of Interface 1") ;
+        }
+
+        void i2.show()
+        {
+            Console.WriteLine("This is method of Interface 2");
+        }
     }
     //class partTimeEmployee : IEmployee
     //{
@@ -26,20 +40,21 @@
     //    }
     //}
 
-    class Program : i2
+    class Program 
     {
-        public void show()
-        {
-            Console.WriteLine("This is a method of interface IEmployee");
-        }
-        public void show1()
-        {
-            Console.WriteLine("This is a method of interface i1");
-        }
-        public void show2() 
-        {
-            Console.WriteLine("This is a method of interface i2");
-        }
+        //public void show()
+        //{
+        //    Console.WriteLine("This is a method of interface IEmployee");
+        //}
+        //public void show1()
+        //{
+        //    Console.WriteLine("This is a method of interface i1");
+        //}
+        //public void show2() 
+        //{
+        //    Console.WriteLine("This is a method of interface i2");
+        //}
+        //................
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
@@ -52,12 +67,23 @@
 
 
             //........
-            i2 myInterface = new Program();
+            //i2 myInterface = new Program();
 
-            myInterface.show();
-            myInterface.show1();
-            myInterface.show2();
+            //myInterface.show();
+            //myInterface.show1();
+            //myInterface.show2();
 
+            //..............
+
+            MyClass myClass = new MyClass();
+            ((i1)myClass).show();
+            ((i2)myClass).show();
+
+            i1 my1 = new MyClass();
+            my1 .show();
+
+            i2 my2 = new MyClass();
+            my2 .show();
         }
     }
 }
