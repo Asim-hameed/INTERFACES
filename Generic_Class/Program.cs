23f12c243909ed4bcf85f1a2c40d98952b3538aa
Asim-hameed <asimhameed31@gmail.com>
2023-04-03 22:39:44 +0500

    Generic Class is implmented using Placeholder Variable to type cast to every type

---
+++ Generic_Class/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Generic_Class
+{
+    class Example<T>
+    {
+        T box;
+
+        public T Box 
+        {
+            set 
+            { 
+                this.box = value;
+            }
+            get { return this.box; }
+
+        }
+        //public Example(T b) 
+        //{
+        //    this.box = b;
+        //}
+        //public T getBox() { return box; }
+
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            //Example<int> example = new Example<int>(20);
+            //Console.WriteLine(example.getBox());
+
+            //Example<string> example1 = new Example<string>("Asim");
+            //Console.WriteLine(example1.getBox());
+
+            //..................
+
+            Example<int>example = new Example<int>();   
+            example.Box = 1;
+            Console.WriteLine(example.Box);
+        }
+    }
+}
