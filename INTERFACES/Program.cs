e51506ad79fd6515499074cf9edd252b989e7cd0
Asim-hameed <asimhameed31@gmail.com>
2023-04-03 19:13:00 +0500

    Interface introduction.

--- INTERFACES/Program.cs
+++ INTERFACES/Program.cs
@@ -1,10 +1,30 @@
 ﻿namespace INTERFACES
 {
-    internal class Program
+    interface IEmployee
+    {
+        // All the members are public by default
+        // Inteface should have signature not body
+        // We cannot have an object of interface but canbe inherited to classes and other interfaces
+        // It cannot contain fields and cannot use acces modifier
+
+
+        void show();
+    }
+    class partTimeEmployee : IEmployee
+    {
+        public void show()
+        {
+            Console.WriteLine("This is a method of Interface IEmployee");
+        }
+    }
+    class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            partTimeEmployee partTimeEmployee = new partTimeEmployee();
+            partTimeEmployee.show();
+
         }
     }
 }
