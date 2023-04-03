b6c9f34a29476b47fb79ba25e14ca233c634b468
Asim-hameed <asimhameed31@gmail.com>
2023-04-03 23:32:06 +0500

    HashTable Collections : Key-Value pair insertion and deletion

---
+++ HashTable/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+namespace HashTableDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable ht = new Hashtable();
+            //{
+            //    { "Id",1123 },
+            //    { "Name", "Asim" },
+            //    { "Age", 25 }
+            //};
+            ht.Add("Id", 1123);
+            ht.Add("Name", "Asim");
+            ht.Add("Salary", 300000);
+            ht.Add("Designation", "PSE");
+            ht.Add("isMarried", false);
+
+            Console.WriteLine(ht["Id"]);
+        }
+    }
+}
