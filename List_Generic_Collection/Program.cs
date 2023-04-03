0632b426df49b62c52e1fa21a248da0799d16131
Asim-hameed <asimhameed31@gmail.com>
2023-04-04 01:00:59 +0500

    List

---
+++ List_Generic_Collection/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace List_Generic_Collection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
