750b4538fede430968bee2950deadc3a504eb026
Asim-hameed <asimhameed31@gmail.com>
2023-04-04 01:35:50 +0500

    List Generic Collection

--- List_Generic_Collection/Program.cs
+++ List_Generic_Collection/Program.cs
@@ -1,10 +1,54 @@
 ﻿namespace List_Generic_Collection
 {
+    // List is Generic while ArrayList is Non-Generic Collection
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Employee emp1 = new Employee()
+            {
+                Name = "Test",
+                Designation = "officer",
+                Id = 1
+            };
+            Employee emp2 = new Employee()
+            {
+                Name = "Test",
+                Designation = "officer",
+                Id = 1
+            };
+            Employee emp3 = new Employee()
+            {
+                Name = "Test",
+                Designation = "officer",
+                Id = 1
+            };
+
+            List<Employee> employees = new List<Employee>();
+            employees.Add(emp1);
+            employees.Add(emp2);
+            employees.Add(emp3);
+
+            foreach (Employee emp in employees)
+            {
+                Console.WriteLine("Employee name is {0} ID is {1} Designation is {2}",emp.Name,emp.Id,emp.Designation);
+            }
+            
+            
+            List<int> ints = new List<int>();
+            ints.Add(1);
+            ints.Add(2);
+            ints.Add(3);
+            ints.Add(4);
+            ints.Add(5);
+            ints.Add(6);
+            Console.WriteLine(ints.Capacity);
+            Console.WriteLine(ints.Count);
+
+            foreach (int i in ints)
+            {
+                Console.WriteLine(i);
+            }
         }
     }
 }
