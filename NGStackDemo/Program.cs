2e2689e70540819aed72c9d6fc21584c445a797c
Asim-hameed <asimhameed31@gmail.com>
2023-04-04 00:10:35 +0500

    Non-Generic Stack with duplicate and null values.

---
+++ NGStackDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+namespace NGStackDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack stack =new Stack();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push("Asim");
+            stack.Push('O');
+            stack.Push(25.75);
+            stack.Push(null);
+            stack.Push(true);
+
+            foreach (Object item in stack)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine(stack.Count);
+            stack.Pop();
+            foreach (Object item in stack)
+            {
+                Console.WriteLine(item);
+            }
+            stack.Peek();
+            Console.WriteLine(stack.Count);
+        }
+    }
+} 
