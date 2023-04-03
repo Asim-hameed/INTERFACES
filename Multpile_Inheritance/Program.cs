adb58679464323a787ae2ab26d886ba5cccc9b16
Asim-hameed <asimhameed31@gmail.com>
2023-04-03 22:04:03 +0500

    Multiple Inheritance using multiple interfaces and with only one class

---
+++ Multpile_Inheritance/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Multpile_Inheritance
+{
+    class Person
+    {
+        public void show1()
+        {
+            Console.WriteLine("This is a method of person class");
+        }
+    }
+    interface Employee
+    {
+        void show2();
+    }
+
+    // Multpile interfaces can be implemented with inheritance
+
+    class Teacher : Person, Employee
+    {
+        public void show2()
+        {
+            Console.WriteLine("This is a method of Employee Interface");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Teacher teacher = new Teacher();
+            teacher.show1();
+            teacher.show2();
+        }
+    }
+}
