0997a7f34d75c939b0182e61764b1eede5a8041d
Asim-hameed <asimhameed31@gmail.com>
2023-04-05 02:01:19 +0500

    Threading via Multitasking

---
+++ MultiThreading_Demo/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace MultiThreading_Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread t = Thread.CurrentThread;
+            t.Name = "Main Thread";
+        }
+    }
+}
