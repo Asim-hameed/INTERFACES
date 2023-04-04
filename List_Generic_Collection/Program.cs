a3764526773dfaf010421a26ae0c6fc481ffd4d5
Asim-hameed <asimhameed31@gmail.com>
2023-04-04 17:47:34 +0500

    List explaination extended

--- List_Generic_Collection/Program.cs
+++ List_Generic_Collection/Program.cs
@@ -1,54 +1,75 @@
 ﻿namespace List_Generic_Collection
 {
     // List is Generic while ArrayList is Non-Generic Collection
+    //List is type safe
     internal class Program
     {
         static void Main(string[] args)
         {
-            Employee emp1 = new Employee()
-            {
-                Name = "Test",
-                Designation = "officer",
-                Id = 1
-            };
-            Employee emp2 = new Employee()
-            {
-                Name = "Test",
-                Designation = "officer",
-                Id = 1
-            };
-            Employee emp3 = new Employee()
-            {
-                Name = "Test",
-                Designation = "officer",
-                Id = 1
-            };
+            //Employee emp1 = new Employee()
+            //{
+            //    Name = "Test",
+            //    Designation = "officer",
+            //    Id = 1
+            //};
+            //Employee emp2 = new Employee()
+            //{
+            //    Name = "Test",
+            //    Designation = "officer",
+            //    Id = 1
+            //};
+            //Employee emp3 = new Employee()
+            //{
+            //    Name = "Test",
+            //    Designation = "officer",
+            //    Id = 1
+            //};
 
-            List<Employee> employees = new List<Employee>();
-            employees.Add(emp1);
-            employees.Add(emp2);
-            employees.Add(emp3);
+            //List<Employee> employees = new List<Employee>();
+            //employees.Add(emp1);
+            //employees.Add(emp2);
+            //employees.Add(emp3);
 
-            foreach (Employee emp in employees)
-            {
-                Console.WriteLine("Employee name is {0} ID is {1} Designation is {2}",emp.Name,emp.Id,emp.Designation);
-            }
+            //foreach (Employee emp in employees)
+            //{
+            //    Console.WriteLine("Employee name is {0} ID is {1} Designation is {2}",emp.Name,emp.Id,emp.Designation);
+            //}
             
             
-            List<int> ints = new List<int>();
-            ints.Add(1);
-            ints.Add(2);
-            ints.Add(3);
-            ints.Add(4);
-            ints.Add(5);
-            ints.Add(6);
-            Console.WriteLine(ints.Capacity);
-            Console.WriteLine(ints.Count);
+            //List<int> ints = new List<int>();
+            //ints.Add(1);
+            //ints.Add(2);
+            //ints.Add(3);
+            //ints.Add(4);
+            //ints.Add(5);
+            //ints.Add(6);
+            //Console.WriteLine(ints.Capacity);
+            //Console.WriteLine(ints.Count);
 
-            foreach (int i in ints)
+            //foreach (int i in ints)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            List<string> list = new List<string>();
+            list.Add("Asim");
+            list.Add("Adil");
+            list.Add("Asim");
+            list.Add(null);
+            list.Add("Talha");
+
+            foreach (string item in list)
             {
-                Console.WriteLine(i);
+                Console.WriteLine(item);
             }
+
+            list.Sort();
+
+            foreach (string item in list)
+            {
+                Console.WriteLine(item);
+            }
+
         }
     }
 }
