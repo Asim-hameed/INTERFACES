2cbd33a6389538ecbaca5c634d0925dec59edc08
Asim-hameed <asimhameed31@gmail.com>
2023-04-05 02:05:52 +0500

    Serial execution of functions

--- MultiThreading_Demo/Program.cs
+++ MultiThreading_Demo/Program.cs
@@ -2,10 +2,36 @@
 {
     internal class Program
     {
+        public static void fun1()
+        {
+            for (int j = 0; j <= 50; j++)
+            {
+                Console.WriteLine("Fun1= "+j);
+            }
+        }
+        public static void fun2()
+        {
+            for (int j = 0; j <= 50; j++)
+            {
+                Console.WriteLine("Fun2= " + j);
+            }
+        }
+        public static void fun3()
+        {
+            for (int j = 0; j <= 50; j++)
+            {
+                Console.WriteLine("Fun3= " + j);
+            }
+        }
         static void Main(string[] args)
         {
-            Thread t = Thread.CurrentThread;
-            t.Name = "Main Thread";
+            Program.fun1();
+            Program.fun2();
+            Program.fun3();
+
+            //Thread t = Thread.CurrentThread;
+            //t.Name = "Main Thread";
+            //Console.WriteLine("Current excecuting thread is "+Thread.CurrentThread.Name);
         }
     }
 }
