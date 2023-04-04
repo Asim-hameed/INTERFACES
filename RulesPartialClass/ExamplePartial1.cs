0c4db96d58b468644a9f0d38da189e53dc404873
Asim-hameed <asimhameed31@gmail.com>
2023-04-05 01:01:54 +0500

    Rules for Partial classes implmented

---
+++ RulesPartialClass/ExamplePartial1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RulesPartialClass
+{
+    public partial class ExamplePartial 
+    {
+        public void show()
+        {
+            Console.WriteLine(name);
+        }
+    }
+}
---
+++ RulesPartialClass/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace RulesPartialClass
+{
+    public class MyClass
+    {
+        public void show()
+        {
+            Console.WriteLine("I am Show");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ExamplePartial partial = new ExamplePartial();
+            //partial.show();
+        }
+    }
+
+    public interface ImyInterface1
+    {
+        void show1();
+    }
+    public interface ImyInterface2
+    {
+        void show2();
+    }
+}
