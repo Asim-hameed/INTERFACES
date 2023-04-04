428598daad6b4e9ea8821276d87a879cd815676d
Asim-hameed <asimhameed31@gmail.com>
2023-04-05 01:26:29 +0500

    Checked Efficiency of String and String builder

--- StringAndStringBuilder/Program.cs
+++ StringAndStringBuilder/Program.cs
@@ -1,10 +1,32 @@
-﻿namespace StringAndStringBuilder
+﻿using System.Text;
+using System.Diagnostics;
+
+namespace StringAndStringBuilder
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            string s1 = "Asim ";
+            Stopwatch sw = Stopwatch.StartNew();
+            sw.Start();
+            for(int i=0; i<100000; i++)
+            {
+                s1=s1+i;
+            }
+            sw.Stop();
+            Console.WriteLine("Time taken by String: "+sw.ElapsedMilliseconds);
+
+
+            sw.Restart();
+            StringBuilder sb = new StringBuilder("Hameed ");
+            for(int i=0; i<100000; i++)
+            {
+                sb.Append(i);
+            }
+            sw.Stop();
+            Console.WriteLine("Time taken by String: " + sw.ElapsedMilliseconds);
+
         }
     }
 }
