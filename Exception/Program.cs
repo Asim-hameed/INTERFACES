2be7b43a26da5715b285b97223d059d20c1d3a8b
Asim-hameed <asimhameed31@gmail.com>
2023-04-04 19:39:49 +0500

    Divide by zero Exception

---
+++ Exception/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Exception
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter First number: ");
+            int num1=int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Second number: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            try
+            {
+                int result = num1 / num2;
+                Console.WriteLine("Division result is: {0}", result);
+            }
+            catch(DivideByZeroException ex)
+            {
+                Console.WriteLine("You cannot divid a number by zero");
+                Console.WriteLine(ex.Message);
+            }
+
+
+
+
+            Console.WriteLine("Remaining statement 1");
+            Console.WriteLine("Remaining statement 2");
+            Console.WriteLine("Remaining statement 3");
+        }
+    }
+}
