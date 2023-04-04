8b2a6995978ea8c723c3513dcd0bc153ae9ebc1e
Asim-hameed <asimhameed31@gmail.com>
2023-04-05 01:10:50 +0500

    Params keyword implemented.

---
+++ ParamsDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ParamsDemo
+{
+    // Every ohter variable rather than param will be assigned the values coming first
+    // It should be at defined at last
+    // No additional param is allowed at last after param
+    internal class Program
+    {
+        public static int add(int a,params int[]num)
+        {
+            int sum = 0;
+            foreach (int i in num)
+            {
+                sum += i;
+            }
+            return sum;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Program.add(10, 5, 19, 74));
+        }
+    }
+}
