7c7b6ba4359acb29dc0f62956cdd71399c1efafb
Asim-hameed <asimhameed31@gmail.com>
2023-04-04 19:01:10 +0500

    Dictionary Generic Collection in key value format

---
+++ Dictionary/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Dictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string,string> myDict = new Dictionary<string,string>();
+            myDict.Add("active", "Ready to engage in physically energetic pursuits");
+            myDict.Add("amazing", "causing great surprise");
+            myDict.Add("qurantine", "place of isolation");
+
+            foreach(KeyValuePair<string,string> kvp in myDict)
+            {
+                Console.WriteLine("Key is "+kvp.Key+" Value is "+kvp.Value);
+            }
+        }
+    }
+} 
