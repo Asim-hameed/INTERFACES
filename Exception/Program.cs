a379cc62ca4ad8e974dbb23f5dfff8f943baeed8
Asim-hameed <asimhameed31@gmail.com>
2023-04-04 19:44:19 +0500

    IndexOutOfRangeException

--- Exception/Program.cs
+++ Exception/Program.cs
@@ -4,28 +4,52 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter First number: ");
-            int num1=int.Parse(Console.ReadLine());
-            Console.WriteLine("Enter Second number: ");
-            int num2 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter First number: ");
+            //int num1=int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter Second number: ");
+            //int num2 = int.Parse(Console.ReadLine());
+
+            //try
+            //{
+            //    int result = num1 / num2;
+            //    Console.WriteLine("Division result is: {0}", result);
+            //}
+            //catch(DivideByZeroException ex)
+            //{
+            //    Console.WriteLine("You cannot divid a number by zero");
+            //    Console.WriteLine(ex.Message);
+            //}
+
+
+
+
+            //Console.WriteLine("Remaining statement 1");
+            //Console.WriteLine("Remaining statement 2");
+            //Console.WriteLine("Remaining statement 3");
+
+
+            //...........
+
+            int[] arr=new int[3];
+            
 
             try
             {
-                int result = num1 / num2;
-                Console.WriteLine("Division result is: {0}", result);
+                arr[0] = 1;
+                arr[1] = 2;
+                arr[2] = 3;
+                arr[3] = 4;
             }
-            catch(DivideByZeroException ex)
+            catch (IndexOutOfRangeException ex)
             {
-                Console.WriteLine("You cannot divid a number by zero");
+                Console.WriteLine("Array's Range is exceeded");
                 Console.WriteLine(ex.Message);
             }
 
-
-
-
-            Console.WriteLine("Remaining statement 1");
-            Console.WriteLine("Remaining statement 2");
-            Console.WriteLine("Remaining statement 3");
+            foreach(int i in arr)
+            {
+                Console.WriteLine(i);
+            }
         }
     }
 }
