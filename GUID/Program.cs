ff19ee07d164d2efc07928f5ea9c1d9e9d119ef0
Asim-hameed <asimhameed31@gmail.com>
2023-04-05 22:58:52 +0500

    GUID

---
+++ GUID/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace GUID
+{
+    internal class Program
+    {
+        
+        // 128 bit(16 bytes)
+        // Unique Value genrator
+        // Use to avoid redundancy
+
+        static void Main(string[] args)
+        {
+            Guid guid = Guid.NewGuid();
+            Console.WriteLine(guid.ToString().Replace("-",string.Empty));
+            Console.WriteLine(guid.ToString("N").Substring(0,11));
+        }
+    }
+}
