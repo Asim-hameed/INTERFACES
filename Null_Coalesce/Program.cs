4760488dc236c0edb701a1d05908a2f8091e7d5a
Asim-hameed <asimhameed31@gmail.com>
2023-04-05 23:26:38 +0500

    Null_Coalesce

---
+++ Null_Coalesce/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Null_Coalesce
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = null;
+            string result = name ?? "Anonymous";
+            Console.WriteLine(result);
+
+            //if(name !=null)
+            //{
+            //    result = name;
+            //    Console.WriteLine(result);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Value not matched");
+            //}
+        }
+    }
+}
