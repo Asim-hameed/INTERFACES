30a53862e2ea2ea83bead7c9960b8c2242e56e07
Asim-hameed <asimhameed31@gmail.com>
2023-04-05 23:10:14 +0500

    Palindrome

---
+++ Palindrome/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Palindrome
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            Console.WriteLine("Enter A String");
+            str = Console.ReadLine();
+
+            char[] arr = str.ToCharArray();
+            Array.Reverse(arr);
+
+            string reverse = new string(arr);
+
+            if(str == reverse)
+            {
+                Console.WriteLine("Palindrome of each other");
+            }
+            else
+            {
+                Console.WriteLine("Not a Plaindrome");
+            }
+        }
+    }
+}
