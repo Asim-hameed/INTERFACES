5d78c65c40449285fc01d4721efb6d7087259abe
Asim-hameed <asimhameed31@gmail.com>
2023-04-05 23:18:25 +0500

    Is to check type of object As to check and cast object type

---
+++ IsVsAs/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace IsVsAs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Is used to check type of object
+
+
+            //object a = "Asim";
+            //bool check = a is string;
+            //Console.WriteLine(check);
+
+
+            // As used to check and cast type of object by assigning value or null to any variable
+
+
+            object a = "Asim";
+            string str=a as string;
+            Console.WriteLine(str);
+
+        }
+    }
+}
