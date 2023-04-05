eb7b54d000450a7369a5d012250b0499d3543eac
Asim-hameed <asimhameed31@gmail.com>
2023-04-05 17:57:42 +0500

    Mutlithreading implmented

--- MultiThreading_Demo/Program.cs
+++ MultiThreading_Demo/Program.cs
@@ -25,9 +25,17 @@
         }
         static void Main(string[] args)
         {
-            Program.fun1();
-            Program.fun2();
-            Program.fun3();
+            Thread t1 = new Thread(fun1);
+            Thread t2 = new Thread(fun2);
+            Thread t3 = new Thread(fun3);
+            t1.Start();
+            t2.Start();
+            t3.Start();
+
+
+            //Program.fun1();
+            //Program.fun2(); 
+            //Program.fun3();
 
             //Thread t = Thread.CurrentThread;
             //t.Name = "Main Thread";
