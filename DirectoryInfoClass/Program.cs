d658d2bc1ed7da0e4f208a7f360b1b72835e46e8
Asim-hameed <asimhameed31@gmail.com>
2023-04-05 22:11:40 +0500

    DirectoryInfo Class implmented

---
+++ DirectoryInfoClass/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+namespace DirectoryInfoClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\ASIM HAMEED\source\repos\INTERFACES\DirectoryInfoClass\My Directory";
+            string path2 = @"C:\Users\ASIM HAMEED\source\repos\INTERFACES\DirectoryInfoClass\My Directory 2";
+
+            DirectoryInfo dir = new DirectoryInfo(path);
+            //dir.Create();
+            //dir.CreateSubdirectory("SubDirectory");
+
+            dir.MoveTo(path2);
+            Console.WriteLine("Directory Created");
+        }
+    }
+}
