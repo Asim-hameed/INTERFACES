47047c711d5ec93c450ee91cbe8292ef512b4c05
Asim-hameed <asimhameed31@gmail.com>
2023-04-06 21:47:57 +0500

    Enums type casting

---
+++ Enums/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Enums
+{
+    class Program
+    {
+        enum Days
+        {
+            sunday,
+            monday,
+            tuesday,
+            wednesday,
+            friday,
+            thursday,
+            saturday
+        }
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("My name is asim");
+
+            Console.WriteLine(Days.saturday);
+
+            Days days = (Days)1;
+
+            Console.WriteLine(days);
+        }
+    }
+}
