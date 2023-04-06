e019a9a3a30d07f4b2eaaef7729f679e7a712946
Asim-hameed <asimhameed31@gmail.com>
2023-04-06 21:56:53 +0500

    Enums Using switch

--- Enums/Program.cs
+++ Enums/Program.cs
@@ -14,15 +14,48 @@
         }
         static void Main(string[] args)
         {
-            Console.BackgroundColor = ConsoleColor.Yellow;
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("My name is asim");
+            //Console.BackgroundColor = ConsoleColor.Yellow;
+            //Console.ForegroundColor = ConsoleColor.Red;
+            //Console.WriteLine("My name is asim");
 
-            Console.WriteLine(Days.saturday);
+            //Console.WriteLine(Days.saturday);
 
-            Days days = (Days)1;
+            //Days days = (Days)1;
 
-            Console.WriteLine(days);
+            //Console.WriteLine(days);
+
+            //.............
+
+
+            Console.WriteLine("Enter your Name:");
+            string name=Console.ReadLine();
+
+            Console.WriteLine("Enter you Day sunday=1");
+            int value = int.Parse(Console.ReadLine());
+
+            Days days = (Days)value;
+
+            switch (days)
+            {
+                case Days.sunday:
+                    break;
+                case Days.monday:
+                    break; 
+                case Days.tuesday:
+                    break;
+                case Days.wednesday:
+                    break;
+                case Days.friday:
+                    break;
+                case Days.thursday:
+                    break;
+                case Days.saturday:
+                    break;
+                default:
+                    break;
+            }
+
+            Console.WriteLine("My name is "+name+" And my birthday is "+days);
         }
     }
 }
