1b46f509a950b613cb700260983e4efac31d073a
Asim-hameed <asimhameed31@gmail.com>
2023-04-06 18:09:21 +0500

    De-Serialize stream of data to object

--- Serialization/Program.cs
+++ Serialization/Program.cs
@@ -6,14 +6,28 @@ namespace Serialization
     {
         static void Main(string[] args)
         {
+            // SERIALIZATION
+
+
+            //string path = "C:\\Users\\ASIM HAMEED\\source\\repos\\INTERFACES\\Serialization\\sample.txt";
+            //Employee employee =new Employee(365,"Asim");
+            //FileStream file = new FileStream(path, FileMode.OpenOrCreate);
+            //BinaryFormatter formatter = new BinaryFormatter();
+            //formatter.Serialize(file, employee);
+            //file.Close();
+            //Console.WriteLine("File Created Successfully");
+            //Console.ReadLine();
+
+            // DE-SERIALIZATION
+
             string path = "C:\\Users\\ASIM HAMEED\\source\\repos\\INTERFACES\\Serialization\\sample.txt";
-            Employee employee =new Employee(365,"Asim");
             FileStream file = new FileStream(path, FileMode.OpenOrCreate);
             BinaryFormatter formatter = new BinaryFormatter();
-            formatter.Serialize(file, employee);
+            Employee employee = (Employee)formatter.Deserialize(file);
+            Console.WriteLine("Employee id: "+employee.id);
+            Console.WriteLine("Employee name: "+employee.name);
             file.Close();
-            Console.WriteLine("File Created Successfully");
-            Console.ReadLine();
+
         }
     }
 }
